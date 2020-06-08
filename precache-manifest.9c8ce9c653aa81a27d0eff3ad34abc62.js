75e7bf0c70925a5e0be8d3fc03359eabde93a8cd
Tushar kumar <tshrkumar9@gmail.com>
2020-06-08 14:53:03 +0530

    Updates

---
+++ precache-manifest.9c8ce9c653aa81a27d0eff3ad34abc62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a870dac4ddbdb0e24f9b5b5dc39825c",
+    "url": "/EdN-project/index.html"
+  },
+  {
+    "revision": "68ab0f2f7d1c28fd0d1e",
+    "url": "/EdN-project/static/css/2.5ec480d0.chunk.css"
+  },
+  {
+    "revision": "d9fe1cd0387982c934a6",
+    "url": "/EdN-project/static/css/main.a5ef490a.chunk.css"
+  },
+  {
+    "revision": "68ab0f2f7d1c28fd0d1e",
+    "url": "/EdN-project/static/js/2.c5985b3f.chunk.js"
+  },
+  {
+    "revision": "d9fe1cd0387982c934a6",
+    "url": "/EdN-project/static/js/main.4725e3dc.chunk.js"
+  },
+  {
+    "revision": "9a1fae48a27c63431a7e",
+    "url": "/EdN-project/static/js/runtime~main.7a3441b2.js"
+  }
+]);
